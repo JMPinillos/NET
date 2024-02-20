923432e3d541b012058f8e1cac56f58d1dc4d370
Jose Manuel Pinillos Rubio <pinillosrubio@gmail.com>
2024-02-20 14:18:26 +0100

    update

---
+++ Aplicaciones/AplicacionesBasicas/AplicacionesBasicas/Circulo.vb
@@ -0,0 +1,14 @@
+﻿Public Class Circulo
+    Inherits Formas
+    
+    Const pi As Double = Math.PI
+    
+    Public Overrides Function perimetro(lado As Object) As Object
+        return 2 * pi * (lado/2)
+    End Function
+
+    Public Overrides Function area(lado As Object) As Object
+        return pi * ((lado/2)^2)
+    End Function
+    
+End Class
---
+++ Aplicaciones/AplicacionesBasicas/AplicacionesBasicas/Main.vb
@@ -0,0 +1,63 @@
+Imports System
+
+Module Main
+    Sub Main(args As String())
+        ' Declarar variable para el lado del cuadrado
+        Dim cuadrado As New Cuadrado
+        Dim circulo As New Circulo
+        Dim triangulo As New Triangulo
+        Dim hexagono As New Hexagono
+        Dim lado As Double
+        '    Dim ladoCuadrado As Double    
+    '    Dim ladoTriangulo As Double
+   '     Dim ladoCirculo As Double
+   '     Dim ladoHexagono As Double
+
+        ' ************************** CUADRADO **************************
+        ' Solicitar al usuario que ingrese el lado del cuadrado
+        Console.Write("Ingrese la longitud del lado del cuadrado: ")
+        
+        ' A la valible ladoCuadrado se le asigna  el valor de entrada del usuario
+        ' transformandolo a Double, porque la entrada del usuario es tratada
+        ' como texto por defecto
+        lado = Convert.ToDouble(Console.ReadLine())
+        
+        ' Mostrar los resultados
+        Console.WriteLine("El perímetro del cuadrado es: " & cuadrado.perimetro(lado))
+        Console.WriteLine("El área del cuadrado es: " & cuadrado.area(lado))
+        
+        ' Esperar a que el usuario presione una tecla antes de continuar
+        Console.WriteLine("Presione cualquier tecla para continuar..." & vbCrLf)
+        Console.ReadKey(True)
+        
+        ' ************************** CÍRCULO **************************
+        Console.Write("Ingrese la longitud del perimetro del circulo: ")
+        lado = Convert.ToDouble(Console.ReadLine())
+
+        Console.WriteLine("El perímetro del círculo es: " & circulo.perimetro(lado))
+        Console.WriteLine("El área del círculo es: " & circulo.area(lado))
+        
+        Console.WriteLine("Presione cualquier tecla para continuar..." & vbCrLf)
+        Console.ReadKey(True)
+        
+        ' ************************** TRIANGULO **************************
+        
+        Console.Write("Ingrese la longitud del perimetro del triángulo: ")
+        lado = Convert.ToDouble(Console.ReadLine())
+
+        Console.WriteLine("El perímetro del triángulo es: " & triangulo.perimetro(lado))
+        Console.WriteLine("El área del triángulo es: " & triangulo.area(lado))
+        
+        Console.WriteLine("Presione cualquier tecla para continuar..." & vbCrLf)
+        Console.ReadKey(True)
+        
+        ' ************************** HEXÁGONO **************************
+        
+        Console.Write("Ingrese la longitud del perimetro del hexágono: ")
+        lado = Convert.ToDouble(Console.ReadLine())
+
+        Console.WriteLine("El perímetro del hexágono es: " & hexagono.perimetro(lado))
+        Console.WriteLine("El área del hexágono es: " & hexagono.area(lado))
+        
+    End Sub
+End Module
